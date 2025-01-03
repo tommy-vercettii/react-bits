451390443824137ed79eb3ee2a1d429342be7e57
David Haz <davidhzdev@gmail.com>
2025-01-03 22:05:25 +0200

    Update: center-align text on splash cursor demo page

--- src/demo/Animations/SplashCursorDemo.jsx
+++ src/demo/Animations/SplashCursorDemo.jsx
@@ -100,7 +100,7 @@ const SplashCursorDemo = () => {
       <TabbedLayout>
         <PreviewTab>
           <Flex overflow="hidden" justifyContent="center" flexDirection={'column'} minH={300} p={0} alignItems="center" className="demo-container" position={'relative'} zIndex={10}>
-            <Text fontSize={'3rem'} opacity={0.5} fontWeight={900} userSelect={'none'}>Move Your Cursor</Text>
+            <Text fontSize={'3rem'} textAlign="center" opacity={0.5} fontWeight={900} userSelect={'none'}>Move Your Cursor</Text>
           </Flex>
           <h2 className="demo-title-extra">Component API</h2>
           <PropTable data={propData} />
