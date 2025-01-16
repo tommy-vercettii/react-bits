0fdb1f1995a6debfc05ee2c51a45e373956057dd
David Haz <davidhzdev@gmail.com>
2025-01-16 11:31:26 +0200

    switch order in main

--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,16 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { customTheme } from './utils/customTheme.js';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
+// attempted fix for syntax highligting render bug
+ReactDOM.createRoot(document.createElement('div')).render(
+  // eslint-disable-next-line react/no-children-prop
+  <SyntaxHighlighter language="" children={''} />
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider theme={customTheme}>
     <HelmetProvider>
       <App />
     </HelmetProvider>
   </ChakraProvider>,
-)
-
-// attempted fix for syntax highligting render bug
-ReactDOM.createRoot(document.createElement('div')).render(
-  // eslint-disable-next-line react/no-children-prop
-  <SyntaxHighlighter language="" children={''} />
 )
