659a6f2b753c49eef92931e9dd82a17d9489478e
David Haz <davidhzdev@gmail.com>
2025-01-28 22:13:50 +0200

    update CLI info for blob cursor

--- src/constants/code/Animations/blobCursorCode.js
+++ src/constants/code/Animations/blobCursorCode.js
@@ -5,7 +5,7 @@ import css from '@content/Animations/BlobCursor/BlobCursor.css?raw';
 import tsCode from '@ts-default/Animations/BlobCursor/BlobCursor.tsx?raw';
 
 export const blobCursor = {
-  ...(generateCliCommands('Animations/BlobCursor', ['default'])),
+  ...(generateCliCommands('Animations/BlobCursor', ['default', 'ts/default'])),
   installation: `npm i @react-spring/web`,
   usage: `import BlobCursor from './BlobCursor'
   
