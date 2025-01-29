986191180ad4d783db1ba91110cd79e3b4575905
whitedevil-glitch <saishankar2803@gmail.com>
2025-01-29 22:37:21 +0530

    updated with the JS+Tailwind version

--- src/constants/code/Components/dockCode.js
+++ src/constants/code/Components/dockCode.js
@@ -2,17 +2,19 @@ import { generateCliCommands } from "@/utils/utils"
 
 import code from "@content/Components/Dock/Dock.jsx?raw"
 import css from "@content/Components/Dock/Dock.css?raw"
+import tailwind from "@tailwind/Components/Dock/Dock.jsx?raw"
 import tsCode from "@ts-default/Components/Dock/Dock.tsx?raw"
 import tsTailwind from "@ts-tailwind/Components/Dock/Dock.tsx?raw"
 
 export const dock = {
-  ...generateCliCommands("Components/Dock"),
+  ...generateCliCommands("Components/Dock",['default', 'ts/default', 'ts/tailwind']),
   installation: `npm i @react-spring/web`,
   usage: `import Dock from './Dock';
 
 <Dock collapsible={false} position="left" responsive="bottom" />`,
   code,
   css,
+  tailwind,
   tsCode,
   tsTailwind,
 }
