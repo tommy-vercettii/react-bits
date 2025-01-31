848e7d06980cea4ab9f704cbf4422ece323b8551
iwint <iwint.cseasnsct@gmail.com>
2025-01-31 09:09:43 +0530

    feat: added ts version for click-spark component in both for tailwind and raw css

--- src/constants/code/Animations/clickSparkCode.js
+++ src/constants/code/Animations/clickSparkCode.js
@@ -2,6 +2,8 @@ import { generateCliCommands } from '@/utils/utils';
 
 import code from '@content/Animations/ClickSpark/ClickSpark.jsx?raw';
 import tailwind from '@tailwind/Animations/ClickSpark/ClickSpark.jsx?raw';
+import tsCode from '@ts-default/Animations/ClickSpark/ClickSpark.tsx?raw'
+import tsTailwind from "@ts-tailwind/Animations/ClickSpark/ClickSpark.tsx?raw";
 
 export const clickSpark = {
   ...(generateCliCommands('Animations/ClickSpark', ['default', 'tailwind'])),
@@ -15,5 +17,7 @@ export const clickSpark = {
   duration={400}
 />`,
   code,
-  tailwind
+  tailwind,
+  tsCode,
+  tsTailwind
 }
