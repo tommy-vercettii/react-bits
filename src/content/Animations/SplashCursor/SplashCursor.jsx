86a6f7395ad0dda8a45ecba48ce029ba408dea5a
David Haz <davidhzdev@gmail.com>
2025-01-31 17:42:45 +0200

    lower resolution for splashcursor's demo

--- src/content/Animations/SplashCursor/SplashCursor.jsx
+++ src/content/Animations/SplashCursor/SplashCursor.jsx
@@ -3,9 +3,9 @@ import { useEffect, useRef } from 'react';
 
 function SplashCursor({
   // You can customize these props if you want
-  SIM_RESOLUTION = 128,
+  SIM_RESOLUTION = 64,
   DYE_RESOLUTION = 1440,
-  CAPTURE_RESOLUTION = 512,
+  CAPTURE_RESOLUTION = 256,
   DENSITY_DISSIPATION = 3.5,
   VELOCITY_DISSIPATION = 2,
   PRESSURE = 0.1,
