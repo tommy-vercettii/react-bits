ee49463f997488cda8ba8a1148282c46e8802322
David Haz <davidhzdev@gmail.com>
2025-02-12 01:07:32 +0200

    Update: remove unused category parameter and enhance sidebar and category styles

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -8,7 +8,7 @@ import { Box } from '@chakra-ui/react';
 import BackToTopButton from '../components/common/BackToTopButton';
 
 const CategoryPage = () => {
-  const { category, subcategory } = useParams();
+  const { subcategory } = useParams();
   const scrollRef = useRef(null);
 
   const SubcategoryComponent = subcategory ? lazy(componentMap[subcategory]) : null;
@@ -22,7 +22,7 @@ const CategoryPage = () => {
       <Helmet>
         <title>React Bits - {decodeLabel(subcategory)}</title>
       </Helmet>
-      <h1 className='main-category'>{decodeLabel(category)}</h1>
+      {/* <h1 className='main-category'>{decodeLabel(category)}</h1> */}
       <h2 className='sub-category'>{decodeLabel(subcategory)}</h2>
       {SubcategoryComponent ? (
         <Suspense>
