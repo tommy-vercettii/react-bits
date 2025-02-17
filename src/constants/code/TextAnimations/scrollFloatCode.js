7e73c38c7b6cbf4584975b2e0ebf5f70bb4e857b
David Haz <davidhzdev@gmail.com>
2025-02-17 23:07:02 +0200

    Update: remove trailing commas from ScrollFloat usage example

--- src/constants/code/TextAnimations/scrollFloatCode.js
+++ src/constants/code/TextAnimations/scrollFloatCode.js
@@ -12,10 +12,10 @@ export const scrollFloat = {
   usage: `import ScrollFloat from './ScrollFloat';
 
 <ScrollFloat
-  animationDuration={1},
-  ease='back.inOut(2)',
-  scrollStart='center bottom+=50%',
-  scrollEnd='bottom bottom-=40%',
+  animationDuration={1}
+  ease='back.inOut(2)'
+  scrollStart='center bottom+=50%'
+  scrollEnd='bottom bottom-=40%'
   stagger={0.03}
 >
   reactbits
