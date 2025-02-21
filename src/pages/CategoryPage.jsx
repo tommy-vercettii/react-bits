5f345f53739fb5a562b368aaa7a387cdfc43a735
David Haz <davidhzdev@gmail.com>
2025-02-21 09:51:47 +0200

    Update: Refactor CategoryPage to simplify code

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -22,17 +22,12 @@ const CategoryPage = () => {
       <Helmet>
         <title>React Bits - {decodeLabel(subcategory)}</title>
       </Helmet>
-      {/* <h1 className='main-category'>{decodeLabel(category)}</h1> */}
+
       <h2 className='sub-category'>{decodeLabel(subcategory)}</h2>
-      {SubcategoryComponent ? (
-        <Suspense>
-          <SubcategoryComponent />
-        </Suspense>
-      ) : (
-        <p className='coming-soon'>This component is work in progress. <br />
-          Follow the project on <a href="https://github.com/DavidHDev/react-bits" target="_blank" rel="noreferrer">GitHub</a> for regular updates.
-        </p>
-      )}
+
+      <Suspense>
+        <SubcategoryComponent />
+      </Suspense>
 
       <BackToTopButton />
     </Box>
