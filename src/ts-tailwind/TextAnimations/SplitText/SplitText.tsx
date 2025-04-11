ca0e3b4108a3128d7b9a5c471e047d33245ad990
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-04-11 17:40:19 -0300

    remove-any-on-SplitText

--- src/ts-tailwind/TextAnimations/SplitText/SplitText.tsx
+++ src/ts-tailwind/TextAnimations/SplitText/SplitText.tsx
@@ -57,7 +57,7 @@ const SplitText: React.FC<SplitTextProps> = ({
         letters.map((_, i) => ({
             from: animationFrom,
             to: inView
-                ? async (next: (props: any) => Promise<void>) => {
+                ? async (next: (props: { opacity: number; transform: string }) => Promise<void>) => {
                     await next(animationTo);
                     animatedCount.current += 1;
                     if (animatedCount.current === letters.length && onLetterAnimationComplete) {
