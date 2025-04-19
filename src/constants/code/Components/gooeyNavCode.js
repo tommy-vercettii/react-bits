c8bbe334a644ef95496fb9ae37bff0620babf0d9
David Haz <davidhzdev@gmail.com>
2025-04-19 21:21:11 +0300

    fix: Update GooeyNav component props for improved particle configuration

--- src/constants/code/Components/gooeyNavCode.js
+++ src/constants/code/Components/gooeyNavCode.js
@@ -20,13 +20,13 @@ const items = [
 <div style={{ height: '600px', position: 'relative' }}>
   <GooeyNav
     items={items}
+    particleCount={15}
+    particleDistances={[90, 10]}
+    particleR={100}
+    initialActiveIndex={0}
     animationTime={600}
-    pCount={15}
-    minDistance={20}
-    maxDistance={42}
-    maxRotate={75}
-    colors={[1, 2, 3, 1, 2, 3, 1, 4]}
     timeVariance={300}
+    colors={[1, 2, 3, 1, 2, 3, 1, 4]}
   />
 </div>`,
   code,
