03e0ad58ba5b6174bc13c11fd04e031e75eef1da
David Haz <davidhzdev@gmail.com>
2025-04-19 21:17:56 +0300

    fix: Clear NEW categories array in Categories.js

--- src/constants/Categories.js
+++ src/constants/Categories.js
@@ -1,5 +1,5 @@
 // Highlighted sidebar items
-export const NEW = ['Dither', 'Animated List', 'Gooey Nav', 'Threads', 'Lightning', 'Folder', 'Text Cursor'];
+export const NEW = [];
 export const UPDATED = [];
 
 // Used for main sidebar navigation
