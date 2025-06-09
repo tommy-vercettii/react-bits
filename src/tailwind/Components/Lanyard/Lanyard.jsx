cc95fa4ee02fc288e3c2f8af6b9d0da3537c05cd
David Haz <davidhzdev@gmail.com>
2025-06-09 22:20:52 +0300

    Update lanyard assets and components

--- src/tailwind/Components/Lanyard/Lanyard.jsx
+++ src/tailwind/Components/Lanyard/Lanyard.jsx
@@ -6,8 +6,9 @@ import { useGLTF, useTexture, Environment, Lightformer } from '@react-three/drei
 import { BallCollider, CuboidCollider, Physics, RigidBody, useRopeJoint, useSphericalJoint } from '@react-three/rapier';
 import { MeshLineGeometry, MeshLineMaterial } from 'meshline';
 
-import cardGLB from "./card.glb";
-import lanyard from "./lanyard.png";
+// replace with your own imports, see the usage snippet for details
+import cardGLB from "../../../assets/lanyard/card.glb";
+import lanyard from "../../../assets/lanyard/lanyard.png";
 
 import * as THREE from 'three';
 
--- src/ts-tailwind/Components/Lanyard/Lanyard.tsx
+++ src/ts-tailwind/Components/Lanyard/Lanyard.tsx
@@ -20,8 +20,9 @@ import {
 import { MeshLineGeometry, MeshLineMaterial } from "meshline";
 import * as THREE from "three";
 
-import cardGLB from "./card.glb";
-import lanyard from "./lanyard.png";
+// replace with your own imports, see the usage snippet for details
+import cardGLB from "../../../assets/lanyard/card.glb";
+import lanyard from "../../../assets/lanyard/lanyard.png";
 
 extend({ MeshLineGeometry, MeshLineMaterial });
 
